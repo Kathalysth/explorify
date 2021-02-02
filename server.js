16c6103ec99c311dbd6de8eedf6917c2f9d4d754
Felipe Stuart <felipe5stuart@gmail.com>
2021-02-01 23:42:28 -0300

    Importante comentario sobre heroku agregado a server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(DB, {
 })
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //process.env.PORT es mandatorio para Heroku
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
