b4c2ffcb82f3df8d8bf64c362f9aee38cb6e231d
Felipe Stuart <felipe5stuart@gmail.com>
2021-02-02 03:40:23 -0300

    Mejor implementacion de stripe 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,8 +55,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id
-    const user = (await User.find({email: session.customer_email})).id
-    const price = session.amount_total / 100
+    const user = (await User.findOne({email: session.customer_email})).id
+    const price = session.amount_total
     await Booking.create({ tour, user, price })
 }
 
